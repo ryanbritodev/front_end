ff3247eecbca53fb0c8655ec76b257206637da4d
Ryan Brito <rm554497@fiap.com.br>
2024-10-02 22:40:38 -0300

    Gemini Funcionando

--- aula13/gemini-front/script.js
+++ aula13/gemini-front/script.js
@@ -15,17 +15,22 @@ form.addEventListener("submit", (event) => {
     event.preventDefault();
     let messageText = inputMessage.value; // Texto do Usuário
     // console.log(messageText)
+    // Mensagem Estruturada para o Gemini
     let structuredMessage = {
         "role": "user",
         "parts": [{"text": messageText}]
     };
 
+    // Adicionando a mensagem ao Array
     messages.push(structuredMessage);
+
+    // Exibindo o Array de Mensagens
     console.log(messages);
 
     // Esvaziar
     inputMessage.value = "";
 
+    // Criando o Elemento da Mensagem do Usuário (DIV)
     const messageElement = document.createElement("div");
     messageElement.classList.add("message");
     messageElement.classList.add("message--sent");
@@ -36,7 +41,8 @@ form.addEventListener("submit", (event) => {
     // Adicionando o elemento pai
     chatlog.appendChild(messageElement);
     
-    // Requisição para sua API Local
+    // Requisição para sua API Local (Back-End)
+    // Promise (Promessa de uma resposta)
     fetch("http://localhost:3000/sendMessage/", {
         method: "POST",
         headers: {
@@ -45,8 +51,20 @@ form.addEventListener("submit", (event) => {
         body: JSON.stringify({
             messages
         })
+    })
+    // then (então) - Promessa da resposta
+    .then(res => res.json())
+    .then(data => {
+        // Data é a resposta da API
+        console.log(data);
+        const messageElement = document.createElement("div");
+        messageElement.classList.add("message");
+        messageElement.classList.add("message--assistant");
+        messageElement.innerHTML = `
+            <div class="message__text">${data.chat_completion}</div>
+    `;
+    chatlog.appendChild(messageElement);
     });
-
 });
 
 // https://ai.google.dev/gemini-api/docs/text-generation?hl=pt-br&lang=node
